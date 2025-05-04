2cd9c966fcdec5304a548f6924136763053abb8f
albus-droid <albinbabuvarghese20@gmail.com>
2025-05-04 14:43:49 +0000

    Added a fn to check if a folder exits
    Also made sure the text files goes to the right directory upon curl

--- get_input.sh
+++ get_input.sh
@@ -12,8 +12,14 @@ url="https://adventofcode.com/$year/day/$day/input"
 #Reading cookie from Lynx.trace
 cookie_string=$(grep -i 'session' Lynx.trace | sed -e 's/^Cookie: //' -e 's/;.*$//' -e 's/^.*session=//' | tr -d '\\r' | tail -n 1)
 
+#Checking if folder exists, if not make one
+if [ ! -d "${year}/day${day}" ]; then
+   mkdir -p "${year}/day${day}" 
+   echo "Dont exist"
+fi
+
 # Send the curl request with the session cookie and save it to a file
-curl -H "Cookie: session=$cookie_string" "$url" -o "${year}_${day}_input.txt"
+curl -H "Cookie: session=$cookie_string" "$url" -o "${year}/day${day}/input.txt"
 
 # Check if curl succeeded and provide feedback
 if [ $? -eq 0 ]; then
